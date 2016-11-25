6a414c045b6f79e665f8c3a3010ec53f14308880
fredvs <fiens@hotmail.com>
2016-11-25 13:49:56 +0100

    Fixed utf8 device info names.

--- src/uos.pas
+++ src/uos.pas
@@ -4669,8 +4669,8 @@ begin
     devinf := Pa_GetDeviceInfo(x);
     apiinf := Pa_GetHostApiInfo(devinf^.hostApi);
 
-    uosDeviceInfos[x].HostAPIName := apiinf^._name;
-    uosDeviceInfos[x].DeviceName := devinf^._name;
+    uosDeviceInfos[x].HostAPIName := UTF8ToAnsi(apiinf^._name);
+    uosDeviceInfos[x].DeviceName := UTF8ToAnsi(devinf^._name);
 
     if x = uosDefaultDeviceIn then
       uosDeviceInfos[x].DefaultDevIn := True
