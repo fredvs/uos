a6a4260e30fc8f6cfce6b6333b2d5578853c42a3
fredvs <fiens@hotmail.com>
2021-02-21 01:00:36 +0100

    Release 2.210221.

--- src/uos.pas
+++ src/uos.pas
@@ -74,7 +74,7 @@ uos_cdrom,
 Classes, ctypes, Math, sysutils;
 
 const
-  uos_version : cint32 = 2201231;
+  uos_version : cint32 = 2210221;
   
 {$IF DEFINED(bs2b)}
   BS2B_HIGH_CLEVEL = (CInt32(700)) or ((CInt32(30)) shl 16);
