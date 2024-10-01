d966854d932a859315a8c81e1de0f19365a02d91
fredvs <fiens@hotmail.com>
2024-10-01 02:08:12 +0200

    Added check if url is valid for webstreaming.(2)

--- examples/uos_httpgetthread.pas
+++ examples/uos_httpgetthread.pas
@@ -12,137 +12,145 @@ unit uos_httpgetthread;
 interface
 
 uses
-  Classes, SysUtils,  Pipes;
+  Classes,
+  SysUtils,
+  Pipes;
 
 type
-  
- { TThreadHttpGetter }
+
+  { TThreadHttpGetter }
 
   TThreadHttpGetter = class(TThread)
   private
     FOutStream: TOutputPipeStream;
-    FWantedURL: String;
-    FIcyMetaInt: Int64;
+    FWantedURL: string;
+    FIcyMetaInt: int64;
     FOnIcyMetaInt: TNotifyEvent;
-    property OnIcyMetaInt: TNotifyEvent read FOnIcyMetaInt write FOnIcyMetaInt; 
+    property OnIcyMetaInt: TNotifyEvent read FOnIcyMetaInt write FOnIcyMetaInt;
     procedure DoIcyMetaInt;
-    function GetRedirectURL(AResponseStrings: TStrings): String;
+    function GetRedirectURL(AResponseStrings: TStrings): string;
     procedure Headers(Sender: TObject);
   protected
     procedure Execute; override;
   public
     FIsRunning: Boolean;
     ICYenabled: Boolean;
-    property IcyMetaInt: Int64 read FIcyMetaInt;
+    property IcyMetaInt: int64 read FIcyMetaInt;
     property IsRunning: Boolean read FIsRunning;
-    constructor Create(AWantedURL: String; AOutputStream: TOutputPipeStream);
-   end;
+    constructor Create(AWantedURL: string; AOutputStream: TOutputPipeStream);
+  end;
 
 implementation
+
 uses
   fphttpclient,
   openssl, { This implements the procedure InitSSLInterface }
   opensslsockets;
 
-{ TThreadHttpGetter }
+           { TThreadHttpGetter }
 
-function TThreadHttpGetter.GetRedirectURL(AResponseStrings: TStrings): String;
+function TThreadHttpGetter.GetRedirectURL(AResponseStrings: TStrings): string;
 var
-  S: String;
-  F: Integer;
-  Search: String = 'location:';
+  S: string;
+  F: integer;
+  Search: string = 'location:';
 begin
   Result := '';
-  for S In AResponseStrings do
+  for S in AResponseStrings do
   begin
-   // WriteLn(S);
+    // WriteLn(S);
     F := Pos(Search, Lowercase(s));
 
     if F > 0 then
     begin
       Inc(F, Length(Search));
-      Exit(Trim(Copy(S, F, Length(S)-F+1)));
+      Exit(Trim(Copy(S, F, Length(S) - F + 1)));
     end;
   end;
 end;
 
-procedure TThreadHttpGetter.DoIcyMetaInt; 
-begin 
-  if Assigned(FOnIcyMetaInt) then 
-    FOnIcyMetaInt(Self); 
-end; 
+procedure TThreadHttpGetter.DoIcyMetaInt;
+begin
+  if Assigned(FOnIcyMetaInt) then
+    FOnIcyMetaInt(Self);
+end;
 
-procedure TThreadHttpGetter.Headers(Sender: TObject ); 
-begin 
-  FIcyMetaInt := StrToInt64Def(TFPHTTPClient(Sender).GetHeader(TFPHTTPClient(Sender).ResponseHeaders, 'icy-metaint'),0); 
-  if (FIcyMetaInt>0) and (FOnIcyMetaInt<>nil) then 
-       Synchronize(@DoIcyMetaInt); 
+procedure TThreadHttpGetter.Headers(Sender: TObject);
+begin
+  FIcyMetaInt := StrToInt64Def(TFPHTTPClient(Sender).GetHeader(TFPHTTPClient(Sender).ResponseHeaders, 'icy-metaint'), 0);
+  if (FIcyMetaInt > 0) and (FOnIcyMetaInt <> nil) then
+    Synchronize(@DoIcyMetaInt);
 end;
 
 procedure TThreadHttpGetter.Execute;
 var
   Http: TFPHTTPClient;
-  URL: String;
+  URL: string;
   err: shortint = 0;
 begin
-  InitSSLInterface;
-  Http := TFPHTTPClient.Create(nil);
-  http.AllowRedirect := true;
-  http.IOTimeout := 2000;  
-  URL := FWantedURL;
-  repeat
-  try
-    Http.RequestHeaders.Clear;
-    if ICYenabled = true then
-    Http.OnHeaders := @Headers; 
-    // writeln(' avant http.get');
-    Http.Get(URL, FOutStream);
-    // writeln(' apres http.get');
-    except
-    on e: EHTTPClient do
-    begin
-      //  writeln(' Http.ResponseStatusCode ' +inttostr(Http.ResponseStatusCode));
-      if (Http.ResponseStatusCode > 399) or (Http.ResponseStatusCode < 1) then // not accessible
-      begin 
-        FIsRunning:=False;
-        break;   
+  URL          := FWantedURL;
+  if pos(' ', URL) > 0 then
+    FIsRunning := False
+  else
+  begin
+    InitSSLInterface;
+    Http       := TFPHTTPClient.Create(nil);
+    http.AllowRedirect := True;
+    http.IOTimeout := 2000;
+    repeat
+      try
+        Http.RequestHeaders.Clear;
+        if ICYenabled = True then
+          Http.OnHeaders := @Headers;
+        // writeln(' avant http.get');
+        Http.Get(URL, FOutStream);
+        // writeln(' apres http.get');
+      except
+        on e: EHTTPClient do
+        begin
+          //  writeln(' Http.ResponseStatusCode ' +inttostr(Http.ResponseStatusCode));
+          if (Http.ResponseStatusCode > 399) or (Http.ResponseStatusCode < 1) then // not accessible
+          begin
+            FIsRunning := False;
+            break;
+          end;
+          if Http.ResponseStatusCode = 302 then
+          begin
+            URL := GetRedirectURL(Http.ResponseHeaders);
+            if URL <> '' then
+              Continue;
+          end
+          else
+            Break;
+          // raise E;
+        end;
+        on e: Exception do
+        begin
+          //  WriteLn(e.Message);
+        end
+        else
+          // Raise;
+          Break;
       end;
-      if Http.ResponseStatusCode = 302 then
-      begin
-        URL := GetRedirectURL(Http.ResponseHeaders);
-        if URL <> '' then Continue;
-      end
-      else
-       Break;
-       // raise E;
-    end;
-    on e: Exception do
-    begin
-    //  WriteLn(e.Message);
-    end
-    else
-     // Raise;
       Break;
-  end;
-  Break;
-  until (False);
-
-  try
-    //FOutStream.Free;
-    Http.Free;
-  finally
-   // make sure this is set to false when done
-    FIsRunning:=False;
+    until (False);
+    try
+      //FOutStream.Free;
+      Http.Free;
+    finally
+      // make sure this is set to false when done
+      FIsRunning := False;
+    end;
   end;
 end;
- 
-constructor TThreadHttpGetter.Create(AWantedURL: String; AOutputStream: TOutputPipeStream);
+
+constructor TThreadHttpGetter.Create(AWantedURL: string; AOutputStream: TOutputPipeStream);
 begin
   inherited Create(True);
-  ICYenabled:=false;
-  FIsRunning:=True;
-  FWantedURL:=AWantedURL;
-  FOutStream:=AOutputStream;
+  ICYenabled := False;
+  FIsRunning := True;
+  FWantedURL := AWantedURL;
+  FOutStream := AOutputStream;
   // Start;
 end;
 
